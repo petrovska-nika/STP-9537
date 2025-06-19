430f4e674293ada05a2e39367b2d87d35786ee69
petrovska-nika <petrovska.nika.dev@gmail.com>
2025-06-20 01:58:21 +0300

    base

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 import './js/burgerMenu';
+import './js/how-the-game-works';
+import './js/design-modes';
+import './js/story-and-characters';
+import './js/visual-gallery';
+import './js/tips-for-players';
+import './js/faq';
