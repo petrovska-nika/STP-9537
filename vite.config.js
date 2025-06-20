5954cb94d29558ab82874450f27d2b397e9cd98b
petrovska-nika <petrovska.nika.dev@gmail.com>
2025-06-20 10:54:27 +0300

    footer

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
+    base: '/STP-9537/',
     root: 'src',
     build: {
       sourcemap: true,
