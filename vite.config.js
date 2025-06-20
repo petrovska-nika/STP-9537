cc29ab5372f7cda665968971c27c4fb876fc3b7f
petrovska-nika <petrovska.nika.dev@gmail.com>
2025-06-20 14:56:41 +0300

    fix1

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,31 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    base: '/STP-9537/',
     root: 'src',
     build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
+      },
       outDir: '../dist',
       emptyOutDir: true,
     },
