541ed7c33efded5eb4a4ce2b3c4ba80c150951ea
anastasiia_obolonska <velentiy@gmail.com>
2025-06-24 13:34:27 +0300

    fix Design-modes

--- src/js/design-modes.js
+++ src/js/design-modes.js
@@ -1,19 +1,46 @@
 import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-const designSwiper = new Swiper('.design-swiper', {
-  slidesPerView: 1.2,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-  },
-  loop: false,
-});
+let swiperInstance;
+
+function initSwiper() {
+  if (window.innerWidth < 768 && !swiperInstance) {
+    swiperInstance = new Swiper('.design-swiper', {
+      modules: [Navigation, Pagination],
+      slidesPerView: 1,
+      spaceBetween: 13,
+      loop: false,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  } else if (window.innerWidth >= 768 && swiperInstance) {
+    swiperInstance.destroy(true, true);
+    swiperInstance = undefined;
+  }
+}
+
+window.addEventListener('load', initSwiper);
+window.addEventListener('resize', initSwiper);
+
+// const gallerySwiper = new Swiper('.design-swiper', {
+//   slidesPerView: 1,
+//   centeredSlides: true,
+//   spaceBetween: 0,
+//   initialSlide: 1,
+//   loop: false,
+//   grabCursor: true,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
