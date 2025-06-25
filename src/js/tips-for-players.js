500a2639f20879fb6f698a8645d05f097bd6e09e
petrovska-nika <petrovska.nika.dev@gmail.com>
2025-06-25 22:51:47 +0300

    tips-fix-nav

--- src/js/tips-for-players.js
+++ src/js/tips-for-players.js
@@ -1,17 +1,19 @@
-import 'swiper/css';
-
-import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/navigation';
 
 let swiperInstance;
 
 function initSwiper() {
   if (window.innerWidth < 1200 && !swiperInstance) {
     swiperInstance = new Swiper('[data-tips-section-swiper]', {
+      modules: [Navigation],
       slidesPerView: 2,
       spaceBetween: 20,
       loop: false,
-      pagination: false,
-      navigation: false,
+      navigation: {
+        nextEl: '[data-tips-btn-next]',
+        prevEl: '[data-tips-btn-prev]',
+      },
     });
   } else if (window.innerWidth >= 1200 && swiperInstance) {
     swiperInstance.destroy(true, true);
